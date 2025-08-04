10e41755379dc118b26a44d79a44e9736124c32b
benjjjO <benjoalex05@gamil.com>
2025-08-04 16:04:15 +0530

    add all the elements ,audio etc

---
+++ index.js
@@ -0,0 +1,33 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input"),
+keyCheckbox=document.querySelector(".keys-checkbox input");
+
+let AllKeys = [],
+  audio = new Audio("tunes/a.wav");
+const playtune = (key) => {
+  audio.src = `tunes/${key}.wav`;
+  audio.play();
+  const clikedkey = document.querySelector(`[data-key="${key}"]`);
+  clikedkey.classList.add("active");
+  setTimeout(() => {
+    clikedkey.classList.remove("active");
+  }, 150);
+};
+
+pianoKeys.forEach((key) => {
+  AllKeys.push(key.dataset.key);
+  key.addEventListener("click", () => playtune(key.dataset.key));
+});
+const handleVolume = (e) =>{
+    audio.volume = e.target.value
+}
+const showHidekeys = ()=>{
+    pianoKeys.forEach(key =>key.classList.toggle("hide"))
+}
+const pressedkey = (e) => {
+  if (AllKeys.includes(e.key)) playtune(e.key);
+};
+
+keyCheckbox.addEventListener("click" ,showHidekeys)
+volumeSlider.addEventListener("input",handleVolume)
+document.addEventListener("keydown", pressedkey);
