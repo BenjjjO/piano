b48e1ad42c8abe68fdcebffec5e25e5b34ea927e
BenjjjO <benjoalex05@gmail.com>
2025-08-30 16:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
-      volumeSlider = document.querySelector(".volume-slider input"),
-      keyCheckbox = document.querySelector(".keys-checkbox input");
+volumeSlider = document.querySelector(".volume-slider input"),
+keyCheckbox=document.querySelector(".keys-checkbox input");
 
 let AllKeys = [],
-    audio = new Audio("tunes/a.wav");
-
+  audio = new Audio("tunes/a.wav");
 const playtune = (key) => {
   audio.src = `tunes/${key}.wav`;
   audio.play();
   const clikedkey = document.querySelector(`[data-key="${key}"]`);
   clikedkey.classList.add("active");
-  setTimeout(() => clikedkey.classList.remove("active"), 150);
+  setTimeout(() => {
+    clikedkey.classList.remove("active");
+  }, 150);
 };
 
-// collect keys + click events
 pianoKeys.forEach((key) => {
   AllKeys.push(key.dataset.key);
   key.addEventListener("click", () => playtune(key.dataset.key));
 });
-
-// volume slider
-const handleVolume = (e) => {
-  audio.volume = e.target.value;
+const handleVolume = (e) =>{
+    audio.volume = e.target.value
+}
+const showHidekeys = ()=>{
+    pianoKeys.forEach(key =>key.classList.toggle("hide"))
+}
+const pressedkey = (e) => {
+  if (AllKeys.includes(e.key)) playtune(e.key);
 };
-volumeSlider.addEventListener("input", handleVolume);
 
-// show/hide keys
-const showHidekeys = () => {
-  pianoKeys.forEach(key => key.classList.toggle("hide"));
-};
-keyCheckbox.addEventListener("click", showHidekeys);
-
-// prevent repeated keydown firing
-const pressedKeys = new Set();
-document.addEventListener("keydown", e => {
-  if (AllKeys.includes(e.key) && !pressedKeys.has(e.key)) {
-    pressedKeys.add(e.key);
-    playtune(e.key);
-  }
-});
-document.addEventListener("keyup", e => pressedKeys.delete(e.key));
+keyCheckbox.addEventListener("click" ,showHidekeys)
+volumeSlider.addEventListener("input",handleVolume)
+document.addEventListener("keydown", pressedkey);
